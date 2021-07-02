bfb575310833193957ee6bcc9285c76ccb2da9cc
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-07-02 18:30:12 +0200

    creating controllers

---
+++ app/Models/Video.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Lesson;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Video extends Model
+{
+    use HasFactory;
+
+    public function lesson(){
+
+        $this->belongsTo(Lesson::class);
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new OrdinalScope());
+    }
+
+    public function hasDownload()
+    {
+        return in_array($this->id, [8, 9]);
+    }
+}
